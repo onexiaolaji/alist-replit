24d8b6ac13f9512662e078ee85b73846a98b6b68
小辣鸡 <51939988+onexiaolaji@users.noreply.github.com>
2024-12-10 10:57:33 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 # rm -rf alist* #Uncomment this line to update
 if [ ! -f "alist" ];then
-  curl -L https://github.com/AlistGo/alist/releases/download/beta/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
+  curl -L https://github.com/AlistGo/alist/releases/download/v3.40.0/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
   tar -zxvf alist.tar.gz
   rm -f alist.tar.gz
 fi
